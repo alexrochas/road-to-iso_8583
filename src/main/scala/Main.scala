394f72a3f7466ea5b4e030c9c9ff5e93c6095873
Alex Rocha <alex.rochas@yahoo.com.br>
2018-08-14 20:52:10 -0300

    feat: some improvement...

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,49 +1,127 @@
 import scodec.bits.{BitVector, _}
-import scodec.codecs._
-import scodec.{Attempt, DecodeResult, _}
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    case class Point(x: Int, y: Int, z: Int)
-
-    val pointCodec = (int8 :: int8 :: int8).as[Point]
-
-    val encoded: Attempt[BitVector] = pointCodec.encode(Point(-5, 10, 1))
-    println(encoded)
-    // Successful(BitVector(24 bits, 0xfb0a01))
-
-    val decoded: Attempt[DecodeResult[Point]] = pointCodec.decode(hex"0xfb0a01".bits)
-    println(decoded)
-    // Successful(DecodeResult(Point(-5, 10, 1), BitVector(empty)))
-
-    case class FirstBitMap(
-                            secondBitMap: Boolean,
-                            pan: Boolean,
-                            processingCode: Boolean,
-                            transactionValue: Boolean,
-                            n5 : Boolean,
-                            n6 : Boolean,
-                            dateTime: Boolean,
-                            n8 : Boolean,
-                            capturedNSU: Boolean
-                          )
-
-    // Second bitmap 0000000000000100
-    println(ByteVector(BigInt("F21A040128C0980A", 16).toByteArray).shiftLeft(8).toBin)
-
-    val bitMap = hex"F21A040128C0980A".bits
-    println(bitMap.toBin)
-    println((bool
-      :: bool
-      :: bool
-      :: bool
-      :: bool
-      :: bool
-      :: bool
-      :: bool
-      :: bool
-      ).as[FirstBitMap].decode(bitMap))
+    val input = "0200F238040100E09008000000000000010016536900512989610902200000000001000002091104266123210604270209051098402950040003124900TECBAN BRAZILTECBAN SUP GOLF CIN    LONDRINA      BRA9861F01C9EDAA87681802610000000015010760000000000011#102@0#103@"
+    println(input.slice(0,4))
+    println(input.slice(4,20))
+    println(input.slice(20,36))
+    println(input.substring(36))
+    println(hex"F238040100E090080000000000000100".bits.toBin)
+    decode(input)
   }
 
+  case class Message()
+
+  object BitMap {
+    val SecondBitMap = 1
+    val Pan = 2
+    val ProcessingCode = 3
+    val TransactionValue = 4
+    val StandardDateTime = 7
+    val CapturedNSU = 11
+    val LocalTime = 12
+    val LocalDate = 13
+    val AccountingDate = 15
+    val InputMode = 22
+    val OperatorCode = 32
+    val SecondCardTrail = 35
+    val OriginNSU = 37
+    val ResponseCode = 39
+    val TerminalId = 41
+    val Establishment = 42
+    val CurrencyCode = 49
+    val EncriptedPassword = 52
+    val PasswordSize = 53
+    val EmmiterChipCrypt = 55
+    val TerminalType = 61
+    val TransactionData = 62
+    val PositiveId = 63
+    val GenericData = 120
+    val ComplementaryData = 122
+    val ResponseNSU = 127
+  }
+
+  def decode(input: String) = {
+    var index = 0
+    val transactionType = input.substring(0, 4)
+    val firstBitMap = BitVector.fromHex(input.slice(4, 20)).get
+    val secondBitMap = BitVector.fromHex(input.slice(20, 36)).get
+    var body = input.substring(36)
+    val pan = if (firstBitMap.toBin.charAt(BitMap.Pan - 1).equals('1')) {
+      val panSize = 2
+      val contentSize = body.slice(index, panSize) // because is LL
+      val content = body.slice(index + panSize, contentSize.toInt + contentSize.length)
+      index = contentSize.toInt + contentSize.length
+      println("Pan " + content)
+      content
+    }
+    val processingCode = if (firstBitMap.toBin.charAt(BitMap.ProcessingCode - 1).equals('1')) {
+      val contentSize = 6 // because is LL
+      val content = body.slice(index, index + 6)
+      index = index + 6
+      println("Processing code " + content)
+      content
+    }
+    val transactionValue = if (firstBitMap.toBin.charAt(BitMap.TransactionValue - 1).equals('1')) {
+      val contentSize = 12 // because is LL
+      val content = body.slice(index, index + 12)
+      index = index + 12
+      println("Transaction value " + content)
+      content
+    }
+    val standardDateTime = if (firstBitMap.toBin.charAt(BitMap.StandardDateTime - 1).equals('1')) {
+      val contentSize = 10 // because is LL
+      val content = body.slice(index, index + 10)
+      index = index + 10
+      println("Standard date time value " + content)
+      content
+    }
+    val nsu = if (firstBitMap.toBin.charAt(BitMap.CapturedNSU - 1).equals('1')) {
+      val contentSize = 6 // because is LL
+      val content = body.slice(index, index + 6)
+      index = index + 6
+      println("NSU value " + content)
+      content
+    }
+    val localTime = if (firstBitMap.toBin.charAt(BitMap.LocalTime - 1).equals('1')) {
+      val contentSize = 6 // because is LL
+      val content = body.slice(index, index + 6)
+      index = index + 6
+      println("Local time value " + content)
+      content
+    }
+    val localDate = if (firstBitMap.toBin.charAt(BitMap.LocalDate - 1).equals('1')) {
+      val contentSize = 4 // because is LL
+      val content = body.slice(index, index + 4)
+      index = index + 4
+      println("Local date value " + content)
+      content
+    }
+    val accountingDate = if (firstBitMap.toBin.charAt(BitMap.AccountingDate - 1).equals('1')) {
+      val contentSize = 4 // because is LL
+      val content = body.slice(index, index + 4)
+      index = index + 4
+      println("Accounting date value " + content)
+      content
+    }
+    val inputMode = if (firstBitMap.toBin.charAt(BitMap.InputMode - 1).equals('1')) {
+      val contentSize = 3 // because is LL
+      val content = body.slice(index, index + 3)
+      index = index + 3
+      println("Input mode value " + content)
+      content
+    }
+    val operatorCode = if (firstBitMap.toBin.charAt(BitMap.OperatorCode - 1).equals('1')) {
+      val operatorCodeSize = 2
+      val contentSize = body.slice(index, index + operatorCodeSize) // because is LL
+      val content = body.slice(index + operatorCodeSize, index + contentSize.toInt + contentSize.length)
+      index = index + operatorCodeSize + contentSize.toInt
+      println("Operator code value " + content)
+      content
+    }
+
+    Message()
+  }
 }
